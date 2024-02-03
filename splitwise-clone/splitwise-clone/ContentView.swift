20febf7799d22ee32c506fc805991f09df85822d
XIANGYU KONG <xiangyukongdeveloper@gmail.com>
2024-02-03 14:17:25 -0500

    update the skeleton code

--- splitwise-clone/splitwise-clone/ContentView.swift
+++ splitwise-clone/splitwise-clone/ContentView.swift
@@ -6,59 +6,25 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-    
-
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        TabView {
+            AddItemView()
+                .tabItem {
+                    Label("Add", systemImage: "book")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            
+            LoginView()
+                .tabItem {
+                    Label("Login", systemImage: "flag")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-        
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
         }
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ splitwise-clone/splitwise-clone/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  splitwise-clone
+//
+//  Created by XIANGYU KONG on 2/3/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("This is Login View Page")
+    }
+}
+
+#Preview {
+    LoginView()
+}
